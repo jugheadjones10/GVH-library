2dc0b4e8932002d0500c94b2caf1ca2c30baa8db
root <kimyoungjin1001@gmail.com>
2021-10-18 06:07:26 +0000

    G

--- src/index.js
+++ src/index.js
@@ -7,6 +7,19 @@ import reportWebVitals from "./reportWebVitals";
 import { ThemeProvider } from "@mui/material/styles";
 import { createTheme } from "@mui/material/styles";
 
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+  dsn: "https://f8737ed4168e41f68784f3ec28a76299@o1042358.ingest.sentry.io/6011347",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
 
 ReactDOM.render(
   <React.StrictMode>
